4cc17ddbc3e6fc730b12eb5f979da3af206476a2
Ayush Jaiswal <ayush@Ayushs-MacBook-Air.local>
2021-04-22 21:41:21 +0530

    Updates

---
+++ precache-manifest.33d9d0a73e701a59fbdb5033d738a7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9317579c644bba7d505f51f9893e66cc",
+    "url": "/table-assignment/index.html"
+  },
+  {
+    "revision": "69c890dcd1a79dd7b39f",
+    "url": "/table-assignment/static/css/main.bffa7f83.chunk.css"
+  },
+  {
+    "revision": "e53442872b3d096fede0",
+    "url": "/table-assignment/static/js/2.be65fd52.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/table-assignment/static/js/2.be65fd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c890dcd1a79dd7b39f",
+    "url": "/table-assignment/static/js/main.9fcf116b.chunk.js"
+  },
+  {
+    "revision": "4beea35138b0052c0a15",
+    "url": "/table-assignment/static/js/runtime-main.b92abbe2.js"
+  }
+]);
